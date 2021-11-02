b93596ca44f5e56e5c67a3fccb18bc0f6d143167
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-11-02 15:39:24 +0100

    da aggiungere il click

--- script.js
+++ script.js
@@ -7,5 +7,41 @@
 // il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
 
 //faccio dire all'utente un numero da uno a tre (sarà il livello di difficoltà del gioco)
-    //se mi dice 1 faccio partire il rimepimento del gioco con la prima difficoltà, se mi dice 2 ... ,se mi dice 3... => un for con un <di tot in base alla difficoltà,con dentro i div con relative classi
+    //se mi dice 1 faccio partire il rimepimento del gioco con la prima difficoltà, se mi dice 2 ... ,se mi dice 3... => un if con tre condizioni con dentro un for con un <di tot in base alla difficoltà,con dentro i div con relative classi
 //quando l'utente clicca su una casella gli faccio capire che è cliccata tramite un cambiamento di classe addEventListener(click,)
+
+let livelloScelto = prompt("scegli il livello di difficoltà tra 1 e 3");
+
+while ((livelloScelto != "1") && (livelloScelto != 2) && (livelloScelto != 3)){
+ livelloScelto = prompt("scegli un numero da 1 e 3 ")
+}
+
+if (livelloScelto === "1"){
+    for (let i = 0; i < 100; i++){ 
+        let creazione = document.getElementById("contenitore")
+    creazione.innerHTML += `
+        <div class="square"></div>
+    `
+    }
+
+}else if(livelloScelto === "2"){
+    for (let i = 0; i < 81; i++){
+        let creazione = document.getElementById("contenitore");
+        creazione.className = "container2";
+        console.log(creazione);
+        creazione.innerHTML += `
+            <div class="square2"></div>
+        `
+    }
+
+}else {
+    for (let i = 0; i < 49; i++){
+        let creazione = document.getElementById("contenitore");
+        creazione.className = "container3 ";
+        console.log(creazione);
+        creazione.innerHTML += `
+            <div class="square3"></div>
+        `
+    }
+}
+
